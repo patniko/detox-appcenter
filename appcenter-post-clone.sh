4e08c9d4942acb2e9f11f2f81fcea84fa21f3ddf
Chad Wilson <chadw@thoughtworks.com>
2018-08-05 16:32:12 +0800

    RN 0.56.0 failing to build/run with node@6 lingering on path; let's clean up.

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,33 +1,27 @@
-echo "Installing applesimutils"
+#!/usr/bin/env bash
+NODE_VERSION=10.8.0
+APPLESIMUTILS_VERSION=0.5.22
+
+echo "Installing applesimutils..."
 mkdir simutils
 cd simutils
-curl https://raw.githubusercontent.com/wix/homebrew-brew/master/AppleSimulatorUtils-0.5.22.tar.gz -o applesimutils.tar.gz
+curl https://raw.githubusercontent.com/wix/homebrew-brew/master/AppleSimulatorUtils-${APPLESIMUTILS_VERSION}.tar.gz -o applesimutils.tar.gz
 tar xzvf applesimutils.tar.gz
 sh buildForBrew.sh
 cd ..
 export PATH=$PATH:./simutils/build/Build/Products/Release
 
-brew install nvm
-source $(brew --prefix nvm)/nvm.sh
-
-echo "Installing Node..."
-nvm install 8.11.3
-nvm use --delete-prefix 8.11.3
-nvm alias default 8.11.3
-
-echo "Detecting applesimutils"
-which applesimutils
-
-echo "Figuring out node version..."
-node --version
+echo "Installing correct node version..."
+export HOMEBREW_NO_AUTO_UPDATE=1
+brew uninstall node@6
+brew install node@8
+brew link node@8 --force --overwrite
 
 echo "Installing dependencies for detox tests..."
 npm install
 
-echo "Building the project..."
+echo "Building the project for Detox tests..."
 npx detox build --configuration ios.sim.release
 
-echo "Executing tests..."
+echo "Executing Detox tests..."
 npx detox test --configuration ios.sim.release --cleanup
-
-
