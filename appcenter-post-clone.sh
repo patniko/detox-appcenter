844afac5099caadfcea1b72ac6122a9ec1979b68
Chad Wilson <chadw@thoughtworks.com>
2018-08-05 16:03:18 +0800

    Upgrade Node version used; simplify to use npx

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -3,18 +3,17 @@ mkdir simutils
 cd simutils
 curl https://raw.githubusercontent.com/wix/homebrew-brew/master/AppleSimulatorUtils-0.5.22.tar.gz -o applesimutils.tar.gz
 tar xzvf applesimutils.tar.gz
-sh buildForBrew.sh 
+sh buildForBrew.sh
 cd ..
 export PATH=$PATH:./simutils/build/Build/Products/Release
 
-echo "Installing NVM..."
 brew install nvm
 source $(brew --prefix nvm)/nvm.sh
 
-echo "Installing v8.5..."
-nvm install v8.5.0
-nvm use --delete-prefix v8.5.0
-nvm alias default v8.5.0
+echo "Installing Node..."
+nvm install 8.11.3
+nvm use --delete-prefix 8.11.3
+nvm alias default 8.11.3
 
 echo "Detecting applesimutils"
 which applesimutils
@@ -22,16 +21,13 @@ which applesimutils
 echo "Figuring out node version..."
 node --version
 
-echo "Installing detox cli..."
-npm install -g detox-cli
-
 echo "Installing dependencies for detox tests..."
 npm install
 
 echo "Building the project..."
-detox build --configuration ios.sim.release
+npx detox build --configuration ios.sim.release
 
 echo "Executing tests..."
-detox test --configuration ios.sim.release --cleanup
+npx detox test --configuration ios.sim.release --cleanup
 
 
